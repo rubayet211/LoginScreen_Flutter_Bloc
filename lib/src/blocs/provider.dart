0e8b9198a0b7730ba3538cdfc16bf947f91b73b2
rubayet211 <rubayet211@gmail.com>
2023-08-08 09:44:53 +0600

    8th commit

---
+++ lib/src/blocs/provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+
+class Provider extends InheritedWidget {
+  final bloc = Bloc();
+
+  Provider({Key? key, Widget? child}) : super(key: key, child: child);
+
+  bool updateShouldNotify(oldWidget) => true;
+
+  static Bloc of(BuildContext context) {
+    return (context.getElementForInheritedWidgetOfExactType() as Provider).bloc;
+  }
+}
